63c90ccf768ab7d4b70729af3fe16bfa287d72e8
Murilo <murilo.b08@aluno.ifsc.edu.br>
2024-03-19 11:59:33 -0300

    Commit IMC(Não finalizado)

---
+++ IMC/IMC/app/src/main/java/com/example/imc/IMCResultado.java
@@ -0,0 +1,53 @@
+package com.example.imc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class IMCResultado extends AppCompatActivity {
+
+    TextView tvResultado;
+
+    ImageView imageViewResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imcresultado);
+
+        tvResultado=findViewById(R.id.tvResultadoIMC);// associei a variável local ao view na tela
+
+        Intent intent = getIntent(); // recuperamos a intenção que veio atividade anterior
+        Bundle bundle = intent.getExtras();//
+        bundle.getString("imc");// recuperamos o valor do imc que foi passado na intenção
+
+        tvResultado.setText(bundle.getString("imc"));// setamos o valor do imc no TextView
+
+        double res = Double.parseDouble(tvResultado.getText().toString());// convertemos o valor do imc para double
+
+        imageViewResultado=findViewById(R.id.ImageViewResultado);// associamos a variável local ao view na tela
+        if(res < 18.5) {
+            imageViewResultado.setImageResource(R.drawable.abaixopeso);
+        }
+        if(res >= 18.5 && res <= 24.9) {
+            imageViewResultado.setImageResource(R.drawable.normal);
+        }
+        if(res >= 25 && res <= 29.9) {
+            imageViewResultado.setImageResource(R.drawable.sobrepeso);
+        }
+        if(res >= 30 && res <= 34.9) {
+            imageViewResultado.setImageResource(R.drawable.obesidade1);
+        }
+        if(res >= 35 && res <= 39.9) {
+            imageViewResultado.setImageResource(R.drawable.obesidade2);
+        }
+        if(res >= 40) {
+            imageViewResultado.setImageResource(R.drawable.obesidade3);
+        }
+    }
+}
--- IMC/IMC/app/src/main/java/com/example/imc/MainActivity.java
+++ IMC/IMC/app/src/main/java/com/example/imc/MainActivity.java
@@ -2,16 +2,35 @@ package com.example.imc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText edl;
+    private EditText edPeso, edAltura;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edl=findViewById(R.id.editmin);
+        edPeso = findViewById(R.id.edPeso);
+        edAltura = findViewById(R.id.edAltura);
+    }
+
+    public void calcularIMC(View view){
+        double peso = Double.parseDouble(edPeso.getText().toString());
+        double altura = Double.parseDouble(edAltura.getText().toString());
+        DecimalFormat df = new DecimalFormat("##.##");
+        double imc = peso / (altura * altura);
+        String resultado = df.format(imc);
+
+        Toast.makeText(this, "IMC: " + resultado, Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(getApplicationContext(), IMCResultado.class);
+        intent.putExtra("imc", resultado);
+        startActivity(intent);
     }
 }
