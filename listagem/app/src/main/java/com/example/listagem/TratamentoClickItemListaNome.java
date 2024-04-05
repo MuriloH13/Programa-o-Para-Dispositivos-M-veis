a29cfd65ea375424efdf6b6e0016f53b5e4446e2
Murilo <murilo.b08@aluno.ifsc.edu.br>
2024-04-05 12:04:02 -0300

    Listagem(Inicio)

---
+++ listagem/app/src/main/java/com/example/listagem/TratamentoClickItemListaNome.java
@@ -0,0 +1,11 @@
+package com.example.listagem;
+
+import android.view.View;
+import android.widget.AdapterView;
+
+public class TratamentoClickItemListaNome implements AdapterView.OnItemClickListener {
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+}
