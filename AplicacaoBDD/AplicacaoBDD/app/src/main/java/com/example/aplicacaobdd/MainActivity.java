6c7d20d09eafc55441406abff8c257975a6783ca
Murilo <murilo.b08@aluno.ifsc.edu.br>
2024-05-13 11:48:03 -0300

    Continuação BDD

--- AplicacaoBDD/AplicacaoBDD/app/src/main/java/com/example/aplicacaobdd/MainActivity.java
+++ AplicacaoBDD/AplicacaoBDD/app/src/main/java/com/example/aplicacaobdd/MainActivity.java
@@ -3,24 +3,41 @@ package com.example.aplicacaobdd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    SQLiteDatabase sqLiteDatabase;
-
+    SQLiteDatabase db;
+    EditText edNome;
+    ListView lista;
+    Button btnCadastrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sqLiteDatabase = openOrCreateDatabase("notas",MODE_PRIVATE,null);
-        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS notas(id INTEGER PRIMARY KEY AUTOINCREMENT, txt TEXT);");
+
+        edNome=findViewById(R.id.txtNome);
+
+        db = openOrCreateDatabase("dbTabela",MODE_PRIVATE,null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS Tabela(id INTEGER PRIMARY KEY AUTOINCREMENT, txt TEXT);");
         String msg = "Hello";
 
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("txt", "Ola Mundo");
+        ContentValues CV = new ContentValues();
+        CV.put("nome", "Valor");
 
-        sqLiteDatabase.insert("notas",null,contentValues);
+        db.insert("tabela",null, CV);
+        db.update("tabela",CV, null, null);
+
+        String s = new String("SQL");
+
+        Cursor c = db.rawQuery(s, null);
+        c.moveToFirst();
+        s=c.getString(c.getColumnIndex("txt"));
     }
+
 }
---
+++ AplicacaoBDD/AplicacaoBDD/app/src/main/java/com/example/aplicacaobdd/Nota.java
@@ -0,0 +1,11 @@
+package com.example.aplicacaobdd;
+
+public class Nota {
+    int id;
+    String txt;
+
+    public Nota(int id, String txt){
+        this.id = id;
+        this.txt = txt;
+    }
+}
